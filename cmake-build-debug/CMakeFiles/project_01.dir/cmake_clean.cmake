bc7446998b420039f7a10f2534d91673c3161fb0
fabrykoppany <koppany.fabry@gmail.com>
2021-03-05 16:38:23 +0200

    Created first project

---
+++ cmake-build-debug/CMakeFiles/project_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_01.dir/main.c.obj"
+  "libproject_01.dll.a"
+  "project_01.exe"
+  "project_01.exe.manifest"
+  "project_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("This is just a test to see if I can use GitHUB and invite collaborators.\n");
+    return 0;
+}
